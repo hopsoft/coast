6f0124447890e6f0fbaceb5912e6e0e30e74b739
Nathan Hopkins <natehop@gmail.com>
2012-11-17 08:39:27 -0700

    Updated all tests to use micro_mock.

--- test/test_coast.rb
+++ test/test_coast.rb
@@ -12,7 +12,7 @@ class TestCoast < MicroTest::Test
   READ_METHODS    = %w(index show)
   MUTATE_METHODS  = %w(create update destroy)
 
-  # Creates a mock ActiveRecord instance.
+  # Creates a mock model instance [ActiveRecord::Base].
   def self.mock_model
     mock = MicroMock.make.new
     mock.stub(:destroy) { @destroyed = true }
@@ -25,36 +25,37 @@ class TestCoast < MicroTest::Test
     mock
   end
 
-  # Creates a mock ActionDispatch::Request instance.
+  # Creates a mock request instance [ActionDispatch::Request].
   def self.mock_request
     mock = MicroMock.make.new
-    mock.stub(:request) { MicroMock.make.new }
+    mock.stub(:request) { @request ||= MicroMock.make.new }
     mock.stub(:params) { @params ||= {} }
     mock.stub(:flash) { @flash ||= {} }
-    # mock.stub(:root_url) { "/" }
     mock
   end
 
-  # Creates a mock ActionController instance.
+  # Creates a mock controller instance [ActionController::Base].
   def self.mock_controller
     mock = MicroMock.make.new
     mock.class.send :include, Coast
     mock.stub(:authorize!) { |*args| @authorize_invoked = true }
+    mock.stub(:authorize_invoked?) { @authorize_invoked }
     mock.stub(:respond_to) { |&block| @responded = true; block.call(format) }
+    mock.stub(:responded?) { @responded }
     mock.stub(:render) { |*args| @performed = @rendered = true }
+    mock.stub(:performed?) { @performed }
+    mock.stub(:rendered?) { @rendered }
     mock.stub(:redirect_to) { |*args| @redirected = true; render }
+    mock.stub(:redirected?) { @redirected }
     mock.stub(:request) { @request ||= TestCoast.mock_request }
     mock.stub(:params) { request.params }
     mock.stub(:flash) { request.flash }
-    mock.stub(:performed?) { @performed }
-    mock.stub(:responded?) { @responded }
-    mock.stub(:rendered?) { @rendered }
     mock.stub(:format) { @format ||= TestCoast.mock_format }
     mock.stub(:root_url) { "/" }
     mock
   end
 
-  # Creates a mock format object.
+  # Creates a mock format instance [ActionController::MimeResponds::Collector].
   def self.mock_format
     mock = MicroMock.make.new
     mock.stub(:html) { |&block| @html = true; block.call if block }
@@ -64,175 +65,141 @@ class TestCoast < MicroTest::Test
   end
 
   before do
-    @mock_model = TestCoast.mock_model
-    @mock_controller = TestCoast.mock_controller
-    @mock_controller.class.set_resourceful_model @mock_model.class
+    @model = TestCoast.mock_model
+    @controller = TestCoast.mock_controller
+    @controller.class.set_resourceful_model @model.class
   end
 
   test ".set_authorize_method" do
-    @mock_controller.class.set_authorize_method :authorize!
-    assert @mock_controller.class.authorize_method == :authorize!
+    @controller.class.set_authorize_method :authorize!
+    assert @controller.class.authorize_method == :authorize!
   end
 
   test ".authorize_method=" do
-    @mock_controller.class.set_authorize_method :authorize!
-    assert @mock_controller.class.authorize_method == :authorize!
+    @controller.class.set_authorize_method :authorize!
+    assert @controller.class.authorize_method == :authorize!
   end
 
   test ".set_resourceful_model" do
     model = Object.new
-    @mock_controller.class.set_resourceful_model model
-    assert @mock_controller.class.resourceful_model == model
+    @controller.class.set_resourceful_model model
+    assert @controller.class.resourceful_model == model
   end
 
   test ".resourceful_model=" do
     model = Object.new
-    @mock_controller.class.resourceful_model = model
-    assert @mock_controller.class.resourceful_model == model
+    @controller.class.resourceful_model = model
+    assert @controller.class.resourceful_model == model
   end
 
   test "set before callbacks" do
     RESTFUL_METHODS.each do |method|
-      assert !@mock_controller.respond_to?("before_#{method}")
-      @mock_controller.class.before(method) {}
-      assert @mock_controller.respond_to?("before_#{method}")
+      assert !@controller.respond_to?("before_#{method}")
+      @controller.class.before(method) {}
+      assert @controller.respond_to?("before_#{method}")
     end
   end
 
   test "set respond_to callbacks" do
     RESTFUL_METHODS.each do |method|
-      assert !@mock_controller.respond_to?("respond_to_#{method}")
-      @mock_controller.class.respond_to(method) {}
-      assert @mock_controller.respond_to?("respond_to_#{method}")
+      assert !@controller.respond_to?("respond_to_#{method}")
+      @controller.class.respond_to(method) {}
+      assert @controller.respond_to?("respond_to_#{method}")
     end
   end
 
   test "set after callbacks" do
     RESTFUL_METHODS.each do |method|
-      assert !@mock_controller.respond_to?("after_#{method}")
-      @mock_controller.class.after(method) {}
-      assert @mock_controller.respond_to?("after_#{method}")
+      assert !@controller.respond_to?("after_#{method}")
+      @controller.class.after(method) {}
+      assert @controller.respond_to?("after_#{method}")
     end
   end
 
   test "RESTful methods exist" do
     RESTFUL_METHODS.each do |method|
-      assert @mock_controller.respond_to?(method)
+      assert @controller.respond_to?(method)
     end
   end
 
   RESTFUL_METHODS.each do |method|
 
     test "<#{method}> responds and renders" do
-      @mock_controller.send(method)
-      assert @mock_controller.responded?
-      assert @mock_controller.rendered?
+      @controller.send(method)
+      assert @controller.responded?
+      assert @controller.rendered?
     end
 
     test "<#{method}> renders for the formats html, xml, json" do
-      @mock_controller.send(method)
-      assert(@mock_controller.format.instance_eval { @html })
-      assert(@mock_controller.format.instance_eval { @xml })
-      assert(@mock_controller.format.instance_eval { @json })
+      @controller.send(method)
+      assert(@controller.format.instance_eval { @html })
+      assert(@controller.format.instance_eval { @xml })
+      assert(@controller.format.instance_eval { @json })
     end
 
-  #     it "invokes the callbacks before, respond_to, after" do
-  #       callbacks = []
+    test "<#{method}> invokes before, respond_to, after callbacks" do
+      callbacks = []
+      @controller.class.before(method) { callbacks << :before }
+      @controller.class.respond_to(method) { callbacks << :respond_to }
+      @controller.class.after(method) { callbacks << :after }
+      @controller.send(method)
+      assert callbacks.include?(:before)
+      assert callbacks.include?(:respond_to)
+      assert callbacks.include?(:after)
+    end
 
-  #       Coast::TestableController.class_eval do
-  #         before(method) { callbacks << :before }
-  #         respond_to(method) { callbacks << :respond_to }
-  #         after(method) { callbacks << :after }
-  #       end
+    test "<#{method}> allows :respond_to callback to perform the render" do
+      @controller.class.respond_to(method) { @performed = true }
+      @controller.send(method)
+      assert @controller.performed?
+      assert !@controller.responded?
+    end
 
-  #       controller = Coast::TestableController.new
-  #       controller.send(method)
-  #       assert callbacks.include?(:before), "Did not invoke the before callback"
-  #       assert callbacks.include?(:respond_to), "Did not invoke the respond_to callback"
-  #       assert callbacks.include?(:after), "Did not invoke the after callback"
-  #     end
+    test "<#{method}> invokes the authorize_method when set" do
+      @controller.class.authorize_method = :authorize!
+      @controller.send(method)
+      assert @controller.authorize_invoked?
+    end
 
-  #     it "allows :respond_to callback to perform the render" do
-  #       Coast::TestableController.respond_to(method) { @performed = true }
-  #       controller = Coast::TestableController.new
-  #       controller.send(method)
-  #       assert controller.responded? == false, "Did not allow :respond_to callback to perform the render"
-  #     end
-
-  #     it "invokes the authorize_method when set" do
-  #       Coast::TestableController.authorize_method = :authorize!
-  #       controller = Coast::TestableController.new
-  #       controller.send(:index)
-  #       assert controller.authorize_invoked?, "Did not invoke the authorize_method"
-  #     end
-
-  #   end
   end
 
-  # ITEM_METHODS.each do |method|
-  #   describe "##{method}" do
-  #     before do
-  #       reset
-  #     end
+  ITEM_METHODS.each do |method|
+    test "<#{method}> allows :before callback to set the resourceful_item" do
+      item = TestCoast.mock_model
+      @controller.class.before(method) { @resourceful_item = item }
+      @controller.send(method)
+      assert @controller.instance_eval { @resourceful_item.object_id } == item.object_id
+    end
 
-  #     it "allows :before callback to set the item" do
-  #       item = Object.new
-  #       Coast::TestableController.before(:index) { @resourceful_item = item }
-  #       controller = Coast::TestableController.new
-  #       controller.index
-  #       assert controller.instance_eval { @resourceful_item } == item, "Did not allow :before callback to set the resourceful_item"
-  #     end
+    test "<#{method}> sets a custom named instance variable for the item" do
+      item = TestCoast.mock_model
+      @controller.class.before(method) { @resourceful_item = item }
+      @controller.send(method)
+      variable = @controller.instance_eval { instance_variable_get(item_instance_var_name) }
+      assert variable.object_id == item.object_id
+    end
+  end
 
-  #     it "sets a custom named instance variable for the item" do
-  #       item = Object.new
-  #       Coast::TestableController.before(:index) { @resourceful_item = item }
-  #       controller = Coast::TestableController.new
-  #       controller.index
-  #       variable = controller.instance_eval { instance_variable_get(item_instance_var_name) }
-  #       assert variable != nil, "Did not set a custom instance variable for the item"
-  #       assert variable == item, "Did not set a custom instance variable for the item to the correct value"
-  #     end
+  MUTATE_METHODS.each do |method|
+    test "<#{method}> redirects" do
+      @controller.send(method)
+      assert @controller.redirected?
+    end
+  end
 
-  #   end
-  # end
+  test "<index> allows :before callback to set the list" do
+    list = TestCoast.mock_model.class.all
+    @controller.class.before(:index) { @resourceful_list = list }
+    @controller.index
+    assert @controller.instance_eval{ @resourceful_list }.object_id == list.object_id
+  end
 
-  # MUTATE_METHODS.each do |method|
-  #   describe "##{method}" do
-  #     before do
-  #       reset
-  #     end
-
-  #     it "redirects" do
-  #       controller = Coast::TestableController.new
-  #       controller.send(method)
-  #       assert controller.redirected?, "Did not redirect"
-  #     end
-  #   end
-  # end
-
-
-  # describe "#index" do
-  #   before do
-  #     reset
-  #   end
-
-  #   it "allows :before callback to set the list" do
-  #     list = [ Object.new ]
-  #     Coast::TestableController.before(:index) { @resourceful_list = list }
-  #     controller = Coast::TestableController.new
-  #     controller.index
-  #     assert controller.instance_eval { @resourceful_list } == list, "Did not allow :before callback to set the resourceful_list"
-  #   end
-
-  #   it "sets a custom named instance variable for the item" do
-  #     list = [ Object.new ]
-  #     Coast::TestableController.before(:index) { @resourceful_list = list }
-  #     controller = Coast::TestableController.new
-  #     controller.index
-  #     variable = controller.instance_eval { instance_variable_get(list_instance_var_name) }
-  #     assert variable != nil, "Did not set a custom instance variable for the item"
-  #     assert variable == list, "Did not set a custom instance variable for the item to the correct value"
-  #   end
-  # end
+  test "<index> sets a custom named instance variable for the item" do
+    list = TestCoast.mock_model.class.all
+    @controller.class.before(:index) { @resourceful_list = list }
+    @controller.index
+    variable = @controller.instance_eval { instance_variable_get(list_instance_var_name) }
+    assert variable.object_id == list.object_id
+  end
 
 end
